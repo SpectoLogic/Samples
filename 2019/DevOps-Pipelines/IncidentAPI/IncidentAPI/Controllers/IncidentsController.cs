995d5e8e020465a297baa31384f6b76345c42ed9
Andreas Pollak <apollak@hotmail.com>
2019-11-28 21:49:26 +0100

    Initial Demo Commit

---
+++ 2019/DevOps-Pipelines/IncidentAPI/IncidentAPI/Controllers/IncidentsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace IncidentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IncidentsController : ControllerBase
+    {
+        private readonly ILogger<IncidentsController> _logger;
+
+        public IncidentsController(ILogger<IncidentsController> logger)
+        {
+            _logger = logger;
+        }
+
+        // https://localhost:44300/api/incidents/23
+        [HttpGet("{incidentId}")]
+        public Incident Get(string incidentId)
+        {
+            return new Incident()
+            {
+                CustomerFirstName = "Hansi",
+                CustomerId = Guid.NewGuid().ToString("D"),
+                CustomerLastName = "Huber",
+                Id = incidentId,
+                Description = "Demo Beschreibung",
+                Subject = "Customer cannot log in"
+            };
+        }
+    }
+}
---
+++ 2019/DevOps-Pipelines/IncidentAPI/IncidentAPI/Incident.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IncidentAPI
+{
+    public class Incident
+    {
+        public string Id { get; set; }
+        public string Subject { get; set; }
+        public string Description { get; set; }
+        public string CustomerId { get; set; }
+        public string CustomerFirstName { get; set; }
+        public string CustomerLastName { get; set; }
+    }
+}
